a7b2a5229c40af63d7151ea518d565c1ec19d026
Vishnu Ravi <vishnu.ravi@gmail.com>
2020-02-28 20:48:39 -0500

    Fix typos

--- routes/index.js
+++ routes/index.js
@@ -8,21 +8,21 @@ app.post("/", function(req, res) {
 
   // patient information
   const patient = {
-    name = data.name,
-    age = data.age,
-    gender = data.gender,
+    name: data.name,
+    age: data.age,
+    gender: data.gender,
   }
 
   // symptom data
   const symptom = {
-    name = data.symptom,
-    description = data.description,
-    onset = data.onset,
-    progression = data.progression,
-    severity = data.severity,
-    associated = data.associated,
-    alleviating = data.alleviating,
-    aggravating = data.aggravating
+    name: data.symptom,
+    description: data.description,
+    onset: data.onset,
+    progression: data.progression,
+    severity: data.severity,
+    associated: data.associated,
+    alleviating: data.alleviating,
+    aggravating: data.aggravating
   }
 
   // generate HPI
